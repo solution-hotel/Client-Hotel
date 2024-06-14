8e2cce264fcc89fb276d327c53b83d102f2915c4
ThangNH23 <huuthang533540403@gmail.com>
2024-06-14 16:46:25 +0700

    thang update

--- src/app/headerTable/page.tsx
+++ src/app/headerTable/page.tsx
@@ -100,7 +100,7 @@ const HeaderTable = () => {
                                 <TableColumn className='border-2 py-5 bg-[#F3E07B]'>Mã Booking</TableColumn>
                                 <TableColumn className='border-2 font-normal bg-[#F3E07B]'>{bookingData.Data?.Id || 'N/A'}</TableColumn>
                                 <TableColumn className='border-2 bg-[#F3E07B]'>Tên khách hàng</TableColumn>
-                                <TableColumn className='border-2 font-normal bg-[#F3E07B]'>{bookingData.Data?.Guest?.LastName} {bookingData.Data?.Guest?.FirstName || 'N/A'}</TableColumn>
+                                <TableColumn className='border-2 font-normal bg-[#F3E07B]'>{bookingData.Data?.Guest?.FirstName} {bookingData.Data?.Guest?.LastName || 'N/A'}</TableColumn>
                             </TableHeader>
                             <TableBody className='justify-center'>
                                 <TableRow key="1">
