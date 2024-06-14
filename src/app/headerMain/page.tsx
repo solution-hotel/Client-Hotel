c957e8a01eb52c7d3c6fe95a1fdd99792f38fea4
ThangNH23 <huuthang533540403@gmail.com>
2024-06-14 09:10:19 +0700

    thang update

--- src/app/headerMain/page.tsx
+++ src/app/headerMain/page.tsx
@@ -1,14 +1,31 @@
+'use client'
+
+import useBookingData from '@/hooks/useBookingData';
+import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
+interface HeaderTableProps {
+  id: number
+}
+
 const HeaderMain = () => {
+  const searchParams = useSearchParams();
+
+  const id = parseInt(searchParams.get("id") || "1", 10);
+  const { bookingData, isLoading, isError } = useBookingData(id);
+
+  const guestName = bookingData?.Data?.Guest?.LastName ? (
+    <b><i>{bookingData.Data.Guest.LastName}</i></b>
+  ) : null ;
+
   return (
     <div className='container border-b border-grey-200 py-6'>
       <div className=" sm:block">
         <div className="font-bold text-3xl sm:pb-0 text-blackish mb-5">
           Thông tin đặt phòng
         </div>
-        <p>xin chào <b><i>Nguyễn Hữu Thắng,</i></b> cảm ơn quý khách đã lựa chọn <b><i className='text-blue'>BlueHouse.</i></b>  <br />
-        Đây là thông tin đặt phòng của bạn</p>
+        <p>Xin chào <b><i>{guestName},</i></b> cảm ơn quý khách đã lựa chọn <b><i className='text-blue'>BlueHouse.</i></b>  <br />
+          Đây là thông tin đặt phòng của bạn</p>
       </div>
     </div>
   )
