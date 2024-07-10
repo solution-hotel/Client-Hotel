f5bc55d94ca9f144f774a5302e82e9fe6b27b6a3
ThangNH23 <huuthang533540403@gmail.com>
2024-07-10 10:11:07 +0700

    thang deploy

--- src/hooks/extraItemsData.ts
+++ src/hooks/extraItemsData.ts
@@ -1,12 +1,12 @@
-// import { fetcher } from "@/app/utils/fetcher";
-// import useSWR from "swr";
+import { fetcher } from "@/app/utils/fetcher";
+import useSWR from "swr";
 
-// const useAdditionalData = (url: string) => {
-//     const { data, error } = useSWR(`http://192.168.1.114:83/extraitems/getall`, fetcher);
+const useAdditionalData = (url: string) => {
+    const { data, error } = useSWR(`http://192.168.1.114:83/extraitems/getall`, fetcher);
 
-//     return {
-//         additionalData: data,
-//         isAdditionalDataLoading: !error && !data,
-//         additionalDataError: error
-//     };
-// }
+    return {
+        additionalData: data,
+        isAdditionalDataLoading: !error && !data,
+        additionalDataError: error
+    };
+}
